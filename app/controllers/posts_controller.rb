bcee292f89d74afc8476bca284729bcf0a1cef24
Daniel Chin <DanChin327@gmail.com>
2024-04-25 09:34:26 +0900

    added post_card scss and also updated countries show page with new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,6 +36,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :body, :image)
   end
 end
